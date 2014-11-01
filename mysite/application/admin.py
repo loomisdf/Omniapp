11dabdd93cb1d93b92d12019501b48a303cf7649
Chris Schipper <schippcj@miamioh.edu>
2014-11-01 14:48:51 -0400

    Modify comment so that they are inline with the application

--- mysite/application/admin.py
+++ mysite/application/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from application.models import Application, Comment
 
+class CommentInline(admin.StackedInline):
+    model = Comment
+    
 class ApplicationAdmin(admin.ModelAdmin):
     fields = ['title', 'description', 'link']
+    inlines = [CommentInline]
 
 
 # Register your models here.
-admin.site.register(Application)
-admin.site.register(Comment)
+admin.site.register(Application, ApplicationAdmin)
