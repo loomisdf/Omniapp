097073b0b685f849be909b9c0177059ce273f940
Daltin Loomis <loomisdf@gmail.com>
2014-11-01 14:58:24 -0400

    rearange comment to title, rating, description

--- mysite/application/admin.py
+++ mysite/application/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from application.models import Application, Comment
 
-class CommentInline(admin.StackedInline):
+class CommentInline(admin.TabularInline):
     model = Comment
+    fields = ['title', 'rating', 'description']
     
 class ApplicationAdmin(admin.ModelAdmin):
     fields = ['title', 'description', 'link']
     inlines = [CommentInline]
-
 
 # Register your models here.
 admin.site.register(Application, ApplicationAdmin)
