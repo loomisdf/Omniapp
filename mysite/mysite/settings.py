d5d8127b690f85243f5c05506b8937bda51f09be
Daltin Loomis <loomisdf@gmail.com>
2014-11-01 12:16:31 -0400

    add application app and imagekit to settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'application',
+    'imagekit',
 )
 
 MIDDLEWARE_CLASSES = (
