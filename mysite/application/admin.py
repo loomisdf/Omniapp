5014b5a1a843b052c85b49f0ad09ccb221097dae
Chris Schipper <schippcj@miamioh.edu>
2014-11-01 14:25:59 -0400

    Add application admin

--- mysite/application/admin.py
+++ mysite/application/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from application.models import Application
 
+class ApplicationAdmin(admin.ModelAdmin):
+    fields = ['title', 'description', 'link']
+
 # Register your models here.
 admin.site.register(Application)
