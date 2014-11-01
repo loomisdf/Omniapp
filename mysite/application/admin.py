a9dbdf4c4e5b6beaa75671b02bb5bd71fcd6393e
Chris Schipper <schippcj@miamioh.edu>
2014-11-01 14:32:27 -0400

    Add comment option for admin

--- mysite/application/admin.py
+++ mysite/application/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from application.models import Application
+from application.models import Application, Comment
 
 class ApplicationAdmin(admin.ModelAdmin):
     fields = ['title', 'description', 'link']
 
+
 # Register your models here.
 admin.site.register(Application)
+admin.site.register(Comment)
