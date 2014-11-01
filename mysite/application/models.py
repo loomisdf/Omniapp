cb52b14b3b62c568870e07da8f3f02fe78a8b363
Daltin Loomis <loomisdf@gmail.com>
2014-11-01 12:17:21 -0400

    create application app and two models: application, and comment

---
+++ mysite/application/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.core.files.storage import FileSystemStorage
+
+fs = FileSystemStorage(location='/media/photos')
+
+# Create your models here.
+
+class Application(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    link = models.CharField(max_length=1000)
+    logo = models.ImageField(storage=fs)
+
+class Comment(models.Model):
+    application = models.ForeignKey(Application)
+    rating = models.IntegerField(default=0)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+                             
+
+
+                             
