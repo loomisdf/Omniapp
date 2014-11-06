e505ea54988d30e8581ac09ffdec49bbf6cab748
Chris Schipper <schippcj@miamioh.edu>
2014-11-06 00:28:10 -0500

    Add app_detail view to views.py

--- mysite/application/views.py
+++ mysite/application/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world, You're at the Application index.")
+
+def app_detail(request, app_id):
+    return HttpResponse("You're looking at %s." % app_id)
