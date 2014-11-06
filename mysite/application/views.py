f197a499ff8aa937aae22b1512cfd479e1119402
Chris Schipper <schippcj@miamioh.edu>
2014-11-06 01:23:40 -0500

    Add index view that lists all applications in the database that link to the app_detail view

--- mysite/application/views.py
+++ mysite/application/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from application.models import Application
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world, You're at the Application index.")
+    app_list = Application.objects.all()
+    context = {'app_list': app_list}
+    return render(request, 'index.html',context)
 
 def app_detail(request, app_id):
     return HttpResponse("You're looking at %s." % app_id)
