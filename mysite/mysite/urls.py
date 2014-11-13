c973f49387b0028bd7f14688edb1c3a0a924361b
Daltin Loomis <loomisdf@gmail.com>
2014-11-13 01:15:36 -0500

    rearrange the urls in the main urls.py, rename some, add signup url

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -5,6 +5,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^application/', include('application.urls')),
+    #url(r'^$', 'home.views.index'),
+    url(r'^$', include('home.urls', namespace="home")),
+    url(r'^signup/', include('user.urls', namespace="signup")),
+    url(r'^application/', include('application.urls', namespace="application")),
     url(r'^admin/', include(admin.site.urls)),
 )
