43c11457e0757bdbfbb646ca5ed2a58c9318a7af
Daltin Loomis <loomisdf@gmail.com>
2014-11-12 21:42:32 -0500

    update models.py with price and logged in

--- mysite/application/models.py
+++ mysite/application/models.py
@@ -15,6 +15,7 @@ class Application(models.Model):
     description = models.TextField(max_length=1000)
     link = models.CharField(max_length=1000)
     rating = CalculateRating()
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     def __str__(self):
         return self.title
     
@@ -30,6 +31,7 @@ class Comment(models.Model):
 class User(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
+    loggedIn = models.BooleanField(default=False)
     def __str__(self):
         return self.username
 
