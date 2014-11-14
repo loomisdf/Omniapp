4fe61a7617d7721503c19089d9e32f6a57d10149
Trace <trace4544@gmail.com>
2014-11-14 09:51:14 -0500

    Add about us page to navigation bar

--- mysite/about_us/urls.py
+++ mysite/about_us/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from about_us import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.about, name='index'),
+    url(r'^$', views.about, name='about_us'),
 )
