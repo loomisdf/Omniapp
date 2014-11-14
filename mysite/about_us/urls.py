c7b45bb79f986e062e3d7bb1883fed3272868615
Trace <trace4544@gmail.com>
2014-11-13 20:22:48 -0500

    create views in about_us

---
+++ mysite/about_us/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, url
+
+from about_us import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.about, name='index'),
+)
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -12,4 +12,5 @@ urlpatterns = patterns('',
     url(r'^signup/', include('user.urls', namespace="signup")),
     url(r'^application/', include('application.urls', namespace="application")),
     url(r'^admin/', include(admin.site.urls)),
+	url(r'^about_us/', include('about_us.urls')),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
