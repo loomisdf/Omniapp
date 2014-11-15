9bb1d1906078354343d2d6a95ee375e470c025da
Daltin <loomisdf@gmail.com>
2014-11-14 20:56:54 -0500

    add static url if not debug

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,3 +14,9 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^about_us/', include('about_us.urls',namespace="about_us")),
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns(
+        '',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
