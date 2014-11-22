f2f2eb97449c4935a2e3401ac3bd50920b8c50a0
loomisdf <loomisdf@gmail.com>
2014-11-22 03:20:15 -0500

    update signup view to include form generation. If the form is filled out properly then a new user model will be saved to the database and the user will be redirected to the created user page

--- user/views.py
+++ user/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.context_processors import csrf
+from user.forms import UserForm
 
 # Create your views here.
-def signup(request):
-    return render_to_response('signup.html')
-
 def login(request):
-    return render_to_response('login.html')
+    return render(request, 'login.html')
+
+def signup(request):
+	form = UserForm()
+	#if this is a post request we need to process the form data
+	if request.method == 'POST':
+		#create a form instance and populate it with data from the request
+		form = UserForm(request.POST)
+		#check whether it's valid:
+		if form.is_valid():
+			#process the data in form.cleaned_data as required
+			# ...
+			# redirect to a new URL:
+			form.save()
+			return HttpResponseRedirect('/signup/created_user/')
+
+		else:
+			form = UserForm()
+
+		return render(request, 'signup.html', {'form': form})
+
+	else:
+		return render(request, 'signup.html', {'form': form})
+
+def created_user(request):
+	return render(request, 'created_user.html')
