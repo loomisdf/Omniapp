c2874beee488d80f0e2ec0f8d85bed3a25a51266
loomisdf <loomisdf@gmail.com>
2014-11-22 03:17:23 -0500

    add created_user url

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,5 @@ from user import views
 urlpatterns = patterns('',
     url(r'^$', views.signup, name='signup'),
     url(r'^login/$', views.login, name='login'),
+    url(r'^created_user/$', views.created_user, name='create_user'),
 )
