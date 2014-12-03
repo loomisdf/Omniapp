e9ab1b810a9812ee95106d3d41f8be5312ba9d6c
Daltin Loomis <loomisdf@gmail.com>
2014-12-03 18:36:55 -0500

    add app_upload and app_upload_complete to urls.py

--- application/urls.py
+++ application/urls.py
@@ -1,8 +1,11 @@
+#urls.py for Application
 from django.conf.urls import patterns, url
 from application import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<app_id>\d+)/$', views.app_detail, name='app_detail'),
+    url(r'^app_upload/$', views.app_upload, name='app_upload'),
+    url(r'^app_upload_complete/$', views.app_upload_complete, name='app_upload_complete'),
 )
 
