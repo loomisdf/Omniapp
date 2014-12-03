68323157930ea9a7503edd0c57c889f246520ddd
Daltin Loomis <loomisdf@gmail.com>
2014-12-03 18:39:45 -0500

    add views for app_upload and for app_upload_complete

--- application/views.py
+++ application/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from application.models import Application, Comment, Supported_Platform
+from application.forms import ApplicationForm
 
 
 # Create your views here.
@@ -19,3 +20,26 @@ def app_detail(request, app_id):
                'supported_platforms_list': supported_platforms_list,
     }
     return render(request,'application.html', context)
+
+def app_upload(request):
+    form = ApplicationForm()
+    print("hello")
+    if request.method == 'POST':
+        form = ApplicationForm(request.POST, request.FILES)
+        if form.is_valid():
+            print('saving form')
+            form.save()
+            return HttpResponseRedirect('/application/app_upload_complete/')
+
+        else: 
+            print("dat form aint valid")
+            print(form.errors)
+            form = ApplicationForm()
+
+        return render(request, 'app_upload.html', {'form': form})
+
+    else: 
+        return render(request, 'app_upload.html', {'form': form})
+
+def app_upload_complete(request):
+    return render(request, 'app_upload_complete.html')
