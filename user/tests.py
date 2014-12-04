0116ecaca9ceab063fb7fe3b488f1efda240a89c
Trace <trace4544@gmail.com>
2014-12-03 20:26:20 -0500

    Creates tests for user

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from user.models import User
+import random
+import unittest
 
-# Create your tests here.
+class AppTestCase(TestCase):
+    def setUp(self):
+        for x in range(0, 1000):
+            User.objects.create(username=str(x), password=str(x),email=str(x))
+    def test_user(self):
+        for x in range(0,1000):
+            temp = User.objects.get(username=str(x))
+            self.assertEqual(temp.username, temp.__str__(),"username did not match temp's username")
+            self.assertEqual(temp.password, str(x),"password did not match temp's password")
+            self.assertEqual(temp.email, str(x),"email did not match temp's email")
+            self.assertIsInstance(temp,User)
